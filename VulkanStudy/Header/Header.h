6a6cd49e1a482188383d565864d6a924164b9065
owuntu <cktao.g@gmail.com>
2022-06-19 21:57:44 +1000

    Vulkan instance creation.

--- VulkanStudy/Header/Header.h
+++ VulkanStudy/Header/Header.h
@@ -3,6 +3,8 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
+#include <vector>
+
 const uint32_t g_WINDOW_WIDTH = 800;
 const uint32_t g_WINDOW_HEIGHT = 600;
 
@@ -29,7 +31,7 @@ private:
 
 	void initVulkan()
 	{
-
+		createInstance();
 	}
 
 	void mainLoop()
@@ -43,10 +45,57 @@ private:
 
 	void cleanup()
 	{
+		vkDestroyInstance(m_instance, nullptr);
+
 		glfwDestroyWindow(m_pWindow);
 		glfwTerminate();
 	}
 
+	void createInstance()
+	{
+		VkApplicationInfo appInfo{};
+		appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+		appInfo.pApplicationName = "Hello Triangle";
+		appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.pEngineName = "No Engine";
+		appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.apiVersion = VK_API_VERSION_1_0;
+
+		VkInstanceCreateInfo createInfo{};
+		createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+		createInfo.pApplicationInfo = &appInfo;
+		
+		uint32_t glfwExtensionCount = 0;
+		const char** glfwExtensions;
+		glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+		createInfo.enabledExtensionCount = glfwExtensionCount;
+		createInfo.ppEnabledExtensionNames = glfwExtensions;
+		createInfo.enabledLayerCount = 0;
+
+		VkResult result = vkCreateInstance(&createInfo, nullptr, &m_instance);
+
+		if (result != VK_SUCCESS)
+		{
+			throw std::runtime_error("Failed to create VK instance!");
+		}
+
+		uint32_t extensionCount = 0;
+		vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+		std::vector<VkExtensionProperties> extensions(extensionCount);
+		vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+
+		std::cout << "Available extensions:\n";
+		
+		for (const auto& extension : extensions)
+		{
+			std::cout << '\t' << extension.extensionName << '\n';
+		}
+	}
+
 private:
 	GLFWwindow* m_pWindow;
+
+	VkInstance m_instance;
 };
